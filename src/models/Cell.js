b60e75927d148f1e0fce538e06af4c96e53e5ecd
Ender2014 <lau.zihao@gmail.com>
2025-02-08 16:41:40 +0800

    feat: finsihed cell class and testing

--- src/models/Cell.js
+++ src/models/Cell.js
@@ -1,3 +1,6 @@
 export default class Cell {
-  constructor() {}
+  constructor() {
+    this.hit = false;
+    this.isShip = false;
+  }
 }
--- src/models/Cell.test.js
+++ src/models/Cell.test.js
@@ -1,12 +1,28 @@
 import Cell from "./Cell";
 
 describe("Cell properties", () => {
-  let ship;
+  let cell;
   beforeEach(() => {
-    ship = new Cell();
+    cell = new Cell();
   });
 
-  test("Should get name", () => {
-    expect(ship.name).toBe("Carrier");
+  test("Should get hit", () => {
+    expect(cell.hit).toBe(false);
+  });
+
+  test("Should get isShip", () => {
+    expect(cell.isShip).toBe(false);
+  });
+
+  test("Should set hit", () => {
+    cell.hit = true;
+    expect(cell.hit).not.toBe(false);
+    expect(cell.hit).toBe(true);
+  });
+
+  test("Should set isShip", () => {
+    cell.isShip = true;
+    expect(cell.isShip).not.toBe(false);
+    expect(cell.isShip).toBe(true);
   });
 });
