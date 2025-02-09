b20333afdabf259835394006ae1896e24db03f48
Ender2014 <lau.zihao@gmail.com>
2025-02-09 15:59:08 +0800

    feat: Added receiveAttack() and testing to gameBoard

--- src/models/GameBoard.js
+++ src/models/GameBoard.js
@@ -1,5 +1,5 @@
 export default class GameBoard {
-  constructor(size) {
+  constructor(size = 0) {
     this.board = this.#createBoard(size);
   }
 
@@ -10,12 +10,25 @@ export default class GameBoard {
     return this.board;
   }
 
-  place(x, y, ship) {
+  placeShip(x, y, ship) {
     const coordinates = Array.from({ length: ship.length }, (_, i) =>
       ship.isHorizontal ? [x, y + i] : [x + i, y]
     );
     coordinates.forEach(([nX, nY]) => {
       this.board[nX][nY] = ship;
     });
+  }
+
+  #isShip(x, y) {
+    return this.board[x][y] !== null;
+  }
+
+  receiveAttack(x, y) {
+    if (!this.#isShip(x, y)) {
+      this.board[x][y] = false;
+      return false;
+    }
+    this.board[x][y].hit(1);
+    return true;
   }
 }
--- src/models/GameBoard.test.js
+++ src/models/GameBoard.test.js
@@ -15,8 +15,8 @@ describe("place() -> placing ships", () => {
     y = 4;
   });
 
-  test("place() should place ships horizontally", () => {
-    gameBoard.place(x, y, ship);
+  test("Should place ships horizontally", () => {
+    gameBoard.placeShip(x, y, ship);
 
     expect(gameBoard.board[x].slice(y, y + ship.length)).toEqual([
       ship,
@@ -25,12 +25,69 @@ describe("place() -> placing ships", () => {
     ]);
   });
 
-  test("place() should place ships vertically", () => {
+  test("Should place ships vertically", () => {
     ship.isHorizontal = false;
-    gameBoard.place(x, y, ship);
+    gameBoard.placeShip(x, y, ship);
 
     expect(
       gameBoard.board.slice(x, x + ship.length).map((row) => row[y])
     ).toEqual([ship, ship, ship]);
+  });
+});
+
+describe("receiveHit() -> registering hits", () => {
+  let gameBoard;
+  let ship1;
+  let ship2;
+
+  beforeEach(() => {
+    ship1 = new Ship("Destroyer", 3, true);
+    ship2 = new Ship("Carrier", 4, true);
+    gameBoard = new GameBoard();
+    gameBoard.board = [
+      [null, null, null, null],
+      [ship1, ship1, ship1, null],
+      [null, null, null, null],
+      [ship2, ship2, ship2, ship2],
+    ];
+  });
+
+  test("Should detect hits  and return true", () => {
+    expect(gameBoard.receiveAttack(1, 0)).toBe(true);
+  });
+
+  test("Should hit the detected ship", () => {
+    gameBoard.receiveAttack(1, 0);
+    expect(ship1.hits).toBe(1);
+  });
+
+  test("Should hit the correct ship", () => {
+    gameBoard.receiveAttack(3, 0);
+    expect(ship1.hits).not.toBe(1);
+    expect(ship2.hits).toBe(1);
+  });
+
+  test("Should be able to handle multiple same-ship hits", () => {
+    Array.from({ length: 3 }, (_, i) =>
+      gameBoard.receiveAttack(3, 0 + i)
+    ).forEach((fn) => fn);
+    expect(ship2.hits).toBe(3);
+  });
+
+  test("Should be able to sink ships", () => {
+    Array.from({ length: 4 }, (_, i) =>
+      gameBoard.receiveAttack(3, 0 + i)
+    ).forEach((fn) => fn);
+    expect(ship2.hits).toBe(4);
+    expect(ship2.isSunk()).toBe(true);
+  });
+
+  test("Should detect misses and return false", () => {
+    expect(gameBoard.receiveAttack(2, 3)).toBe(false);
+  });
+
+  test("Should record missed hits", () => {
+    gameBoard.receiveAttack(2, 3);
+    expect(gameBoard.board[2][3]).toBe(false);
   });
 });
